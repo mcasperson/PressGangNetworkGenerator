af6337118aba3fbc62560f694fe66314b6ca66c0
Matthew Casperson <matthewcasperson@gmail.com>
2013-08-09 09:44:47 +1000

    Updated script

--- vis.sh
+++ vis.sh
@@ -1,18 +1,14 @@
 cd /root/PressGangNetworkGenerator
-rm /tmp/*.rsf
-rm /tmp/*.lay
+rm /tmp/topics.rsf
 rm /tmp/extradata.js
-node index.html
-rm /var/www/html/visualizations/backup/*
-cp /var/www/html/visualizations/*.lay /var/www/html/visualizations/backup
-cp /var/www/html/visualizations/*.rsf /var/www/html/visualizations/backup
-cp /var/www/html/visualizations/extradata.js /var/www/html/visualizations/backup
-for f in /tmp/*.rsf; 
-do
-	echo "Processing ${f}"
-	/usr/java/latest/bin/java -cp CCVisu.jar org.sosy_lab.ccvisu.CCVisu -vertRepu -i $f -outformat LAY -dim 3 > $f.lay 
-done
-cp /tmp/*.lay /var/www/html/visualizations/
-cp /tmp/*.rsf /var/www/html/visualizations/
+node generate.js
+rm /var/www/html/visualizations/topics.lay.old
+rm /var/www/html/visualizations/extradata.js.old
+rm /var/www/html/visualizations/topics.rsf.old
+cp /var/www/html/visualizations/topics.lay /var/www/html/visualizations/topics.lay.old
+cp /var/www/html/visualizations/extradata.js /var/www/html/visualizations/extradata.js.old
+cp /var/www/html/visualizations/topics.rsf /var/www/html/visualizations/topics.rsf.old
+/usr/java/latest/bin/java -cp CCVisu.jar org.sosy_lab.ccvisu.CCVisu -vertRepu -i /tmp/topics.rsf -outformat LAY -dim 3 > /tmp/topics.lay
+cp /tmp/topics.lay /var/www/html/visualizations/
 cp /tmp/extradata.js /var/www/html/visualizations/
-
+cp /tmp/topics.rsf /var/www/html/visualizations/
