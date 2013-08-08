c793abb793f72c6894d35e655972e3d47a84e0b1
Matthew Casperson <matthewcasperson@gmail.com>
2013-08-08 12:06:03 +1000

    Changed layout generation

--- generate.sh
+++ generate.sh
@@ -1,1 +1,1 @@
-java -cp ~/Applications/CCVisu/bin/CCVisu.jar ccvisu.CCVisu topics.rsf -outformat -LAY -dim 3 > topics.lay
+java -cp ~/Applications/CCVisu/bin/CCVisu.jar ccvisu.CCVisu  -i topics.rsf -outformat LAY -dim 3 > topics.lay
